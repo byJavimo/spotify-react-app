dc7dbdab6380dbbe69c38464358b559b20d63aa3
Javier Mora <javier.mora@pfsgroups-MacBook-Pro.local>
2021-06-30 17:55:57 +0200

    Add search icon to search bar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
 import './SearchBar.scss';
 
@@ -22,7 +23,13 @@ const SearchBar = () => {
     return (
         <form className='search-bar' noValidate autoComplete="off">
             <TextField id="outlined-basic" className="search-input" label="Search track" variant="outlined" value={item} onChange={handleChange}/>
-            <Button onClick={searchItem} className='search-button' variant="contained" color="primary"> Search </Button>
+            <Button onClick={searchItem}
+                    startIcon={<SearchIcon />}
+                    className='search-button'
+                    variant="contained"
+                    color="primary">
+                         Search 
+            </Button>
         </form>
     
     )

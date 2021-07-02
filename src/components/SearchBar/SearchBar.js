5c8acbc9f5eafcf622ad0e4d8673f1661e14fdd5
Javier Mora <javier.mora@pfsgroups-MacBook-Pro.local>
2021-07-02 13:00:49 +0200

    Collapsible and pagination. Search track, album, artist and playlist

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -12,6 +12,10 @@ import IconButton from '@material-ui/core/IconButton';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import Grid from '@material-ui/core/Grid';
 import Pagination from '@material-ui/lab/Pagination';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import ApiService from "../../services/apiService.js";
 
@@ -22,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
       '& > *': {
         justifyContent: 'center',
         margin: theme.spacing(2)
-      },
+      }
     },
   }));
 
@@ -30,31 +34,121 @@ const SearchBar = () => {
     const classes = useStyles();
     const [item, setItem] = useState('');
     const [tracks, setTracks] = useState({});
-    const [trackItems, setTrackItems] = useState([])
+    const [playlists, setPlaylists] = useState({});
+    const [albums, setAlbums] = useState({});
+    const [artists, setArtists] = useState({});
+    const [page, setPage] = useState(1);
 
     const handleChange = (event) => {
         setItem(event.target.value);
     }
 
+    const handlePagination = (event, value) => {
+        const pageLimit = 5;
+        searchItems(pageLimit * value);
+        setPage(value);
+    }
+
+    const renderTypeBlock = (id, type) => {
+        if (type?.items?.length > 0) {
+            return (
+                <GridList cellHeight={320} cols={3}>
+                        {type?.items?.map((item) => (
+                            
+                            <GridListTile key={item.id} cols={1}>
+                                {
+                                    id === 'tracks' ? 
+                                    <>
+                                       <img src={item.album?.images[0].url} alt={item.name} />
+                                        <GridListTileBar
+                                                title={item.name}
+                                                subtitle={<span>by: {getArtistsName(item.artists)}</span>}
+                                                actionIcon={
+                                                <IconButton aria-label={`info about ${item.name}`}>
+                                                    <ArrowForwardIosIcon />
+                                                </IconButton>
+                                        }
+                                        />
+                                    </>
+                                    : ''
+                                }
+                                {
+                                    id === 'artists' ? 
+                                    <>
+                                       <img src={item.images[0].url} alt={item.name} />
+                                        <GridListTileBar
+                                                title={item.name}
+                                                actionIcon={
+                                                <IconButton aria-label={`info about ${item.name}`}>
+                                                    <ArrowForwardIosIcon />
+                                                </IconButton>
+                                        }
+                                        />
+                                    </>
+                                    : ''
+                                }
+                                {
+                                    id === 'albums' ? 
+                                    <>
+                                       <img src={item?.images[0].url} alt={item.name} />
+                                        <GridListTileBar
+                                                title={item.name}
+                                                actionIcon={
+                                                <IconButton aria-label={`info about ${item.name}`}>
+                                                    <ArrowForwardIosIcon />
+                                                </IconButton>
+                                        }
+                                        />
+                                    </>
+                                    : ''
+                                }
+                                {
+                                    id === 'playlists' ? 
+                                    <>
+                                       <img src={item?.images[0].url} alt={item.name} />
+                                        <GridListTileBar
+                                                title={item.name}
+                                                subtitle={<span>by: {item.description}</span>}
+                                                actionIcon={
+                                                <IconButton aria-label={`info about ${item.name}`}>
+                                                    <ArrowForwardIosIcon />
+                                                </IconButton>
+                                        }
+                                        />
+                                    </>
+                                    : ''
+                                }
+                             
+                            </GridListTile>
+                            ))}
+                    </GridList>
+            )
+        }
+    }
+
+    const hasResults = (data) => {
+        return data.items?.length > 0
+    }
+
     const getArtistsName = (artists) => {
-        return artists.map( artist => artist.name).toString()
+        return artists?.map( artist => artist.name).toString()
     }
 
     const getPages = (tracks) => {
         return (tracks.total/tracks.limit).toFixed();
     }
 
-    const searchItem = async () => {
+    const searchItems = async (paginationOffSet) => {
         if (item) {
             try {
-                const response = await ApiService.searchTracks(item);
-                if (!response || !response.data.tracks || response.data.tracks.items.length === 0) {
-                    alert('No se han encontrado resultados para esta búsqueda');
+                const response = await ApiService.searchTracks(item, paginationOffSet);
+                if (!response) {
+                        alert('No se han encontrado resultados para esta búsqueda');
                 } else {
                     setTracks(response.data.tracks);
-                    console.log(response.data.tracks);
-                    setTrackItems(response.data.tracks.items);
-                    setItem('');
+                    setPlaylists(response.data.playlists);
+                    setAlbums(response.data.albums);
+                    setArtists(response.data.artists);
                 }
             } catch(error) {
                 console.error(error);
@@ -71,7 +165,7 @@ const SearchBar = () => {
                 <Grid item xs={12}>
                     <form className='search-bar' noValidate autoComplete="off">
                         <TextField id="outlined-basic" className="search-input" label="Search track" variant="outlined" value={item} onChange={handleChange}/>
-                        <Button onClick={searchItem}
+                        <Button onClick={() => searchItems()}
                                 startIcon={<SearchIcon />}
                                 className='search-button'
                                 variant="contained"
@@ -81,33 +175,89 @@ const SearchBar = () => {
                     </form>
                 </Grid>
                 {
-                    trackItems.length > 0 ? 
-                    
-                    <GridList cellHeight={640} item>
-                    <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-                    </GridListTile>
-                        {trackItems.map((item) => (
-                            <GridListTile key={item.id}>
-                                <img src={item.album.images[0].url} alt={item.name} />
-                                <GridListTileBar
-                                        title={item.name}
-                                        subtitle={<span>by: {getArtistsName(item.artists)}</span>}
-                                        actionIcon={
-                                            <IconButton aria-label={`info about ${item.name}`}>
-                                                <ArrowForwardIosIcon />
-                                            </IconButton>
-                                }
-                                />
-                            </GridListTile>
-                            ))}
-                    </GridList>
-                    : ''
+                    !hasResults(tracks) ? '' :
+                        <>
+                            <Grid item xs={12}>
+                            <Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="tracks"
+                                    id="tracks"
+                                >
+                                    Tracks
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    {/* Renderizar bloque de canciones */}
+                                    {renderTypeBlock('tracks',tracks)}
+                                </AccordionDetails>
+                            </Accordion>
+                        </Grid>
+                    </>
                 }
-
+                {
+                    !hasResults(artists) ? '' :
+                    <>
+                    <Grid item xs={12}>
+                            <Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="artists"
+                                    id="artists"
+                                >
+                                    Artists
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    {/* Renderizar bloque de artistas */}
+                                    {renderTypeBlock('artists',artists)}
+                                </AccordionDetails>
+                            </Accordion>
+                        </Grid>
+                    </>
+                }
+                {
+                    !hasResults(albums) ? '' :
+                    <>
+                    <Grid item xs={12}>
+                            <Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="albums"
+                                    id="albums"
+                                >
+                                    Albums
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    {/* Renderizar bloque de albums */}
+                                    {renderTypeBlock('albums',albums)}
+                                </AccordionDetails>
+                            </Accordion>
+                        </Grid>
+                    </>
+                }
+                {
+                    !hasResults(playlists) ? '' :
+                    <>
+                    <Grid item xs={12}>
+                            <Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="playlists"
+                                    id="playlists"
+                                >
+                                    Playlists
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    {/* Renderizar bloque de artistas */}
+                                    {renderTypeBlock('playlists',playlists)}
+                                </AccordionDetails>
+                            </Accordion>
+                        </Grid>
+                    </>
+                }
             </Grid>
             <div className="pagination-bar">
                 {getPages(tracks) > 0 ?
-                    <Pagination className={classes.pagination} count={getPages(tracks)} color="primary" size="large" />
+                    <Pagination className={classes.pagination} count={getPages(tracks)} page={page} onChange={handlePagination} showFirstButton showLastButton color="primary" size="large" />
       
                     :''
                 }
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 class ApiService  {
     constructor() {
-        this.token = 'BQD5x56CPz7-YY6MW_gN_1AQTvAKjGyPHUeBwn3v-mefMMpNoZrpmJngaMqrTPysVlXgYpMZgKTIllmXIWPzN-DAC5HgPmrvkyNHY1_ko_Y2udq6I3ArZCTCRGE-jBhOOuujctCzGWHtqS8B';
+        // It is needed to refresh the token each hour for teh application to work: https://developer.spotify.com/console/get-search-item/?q=&type=&market=&limit=&offset=&include_external=
+        this.token = 'BQCKhbgAsLbSbM1DOJ9QyzZablxH7k7xWKga4MCe2VrCT4tP9BM4GyYa5XMfnz7N1k2q6gq6pCPXcdhG1mCJF6dy1pbyUpoiiAwOSbFYlLfQfoWxktmMM3uBPpWUTcOQTXoVu6Xbtr0pzVBF';
     }
 
-    async searchTracks (query) {
-        return axios.get(`https://api.spotify.com/v1/search?q=${query}&type=track`, {headers: {Authorization: 'Bearer ' + this.token}});
+    async searchTracks (query, paginationOffset = 0) {
+        const limit = 5;
+        return axios.get(`https://api.spotify.com/v1/search?q=${query}&type=track,album,playlist,artist&offset=${paginationOffset}&limit=${limit}`, {headers: {Authorization: 'Bearer ' + this.token}});
     }
 }
 
